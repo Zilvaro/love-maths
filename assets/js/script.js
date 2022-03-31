8224fb79e51b3b18111b862beee4f6682f8a2dfa
Zilvaro <zilvinas@digitalcase.ch>
2022-03-31 18:17:30 +0000

    Add code to increment score or incorrect answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,8 +49,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert(`Hey! You got it right! :D`);
+        incrementScore();
     } else {
         alert(`Awwww... you answered ${userAnswer}, but right answer is ${calculatedAnswer[0]}`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -73,12 +75,20 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Get the current score from DOM and increment it by 1
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Get the current 'incorrect' value from DOM and increment it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
